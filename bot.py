31ebc5cc0aeccdfea2660b4a9369a9b9c12d72ef
TanyaPeshkova <peshkova-2002.01@mail.ru>
2024-10-16 16:36:29 +0700

    Добавление ответов на базовые вопросы

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Добро пожаловать!")
+    bot.reply_to(message, "Добро пожаловать! Как я могу помочь Вам?")
 
 @bot.message_handler(commands=['help'])
 def send_help(message):
@@ -14,7 +14,17 @@ def send_help(message):
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.reply_to(message, message.text)
+    user_message = message.text.lower()
 
+    if 'как дела' in user_message:
+        bot.reply_to(message, "У меня всё отлично, спасибо! А у Вас?")
+    elif 'что делаешь' in user_message or 'чем занимаешься' in user_message:
+        bot.reply_to(message, "Я здесь, чтобы помочь Вам! А Вы чем занимаетесь?")
+    elif 'привет' in user_message or 'здравствуй' in user_message:
+        bot.reply_to(message, "Здавствуйте! Как я могу помочь Вам?")
+    elif 'пока' in user_message or 'до свидания' in user_message:
+        bot.reply_to(message, "До свидания!")    
+    else:
+        bot.reply_to(message, "Извините, я Вас не понимю")
 if __name__ == '__main__':
     bot.polling()
